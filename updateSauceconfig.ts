bab14d0a4753a2c9072645bf79ed6760a93a8350
Rajesh Yarlagadda <axl.rajesh@gmail.com>
2023-06-26 22:59:31 +0100

    Update updateSauceconfig.ts

--- updateSauceconfig.ts
+++ updateSauceconfig.ts
@@ -2,35 +2,30 @@
 import fs from 'fs';
 import yaml from 'js-yaml';
 
-export async function updateSauceConfig(propertyToUpdate: string, propertyValue: string | string[]): Promise<void> {
-    try {
-        // read the yaml file
-        let file: string = fs.readFileSync('./path/to/your/config.yaml', 'utf8');
+type Value = string | string[];  // This is the type for propertyValue now
 
-        // convert the yaml file to javascript object
-        let config: any = yaml.load(file);
+const doc: SauceConfig = yaml.load(fs.readFileSync(ymlconfiguration,'utf8')) as SauceConfig;
 
-        // update the property in the config object
-        if (Array.isArray(propertyValue)) {
-            if (Array.isArray(config[propertyToUpdate])) {
-                // if the property value is an array and the existing property value is also an array, merge them
-                config[propertyToUpdate] = [...config[propertyToUpdate], ...propertyValue];
-            } else {
-                // if the property value is an array but the existing property value is not an array, replace it
-                config[propertyToUpdate] = propertyValue;
-            }
-        } else {
-            // if the property value is not an array, replace the existing property value
-            config[propertyToUpdate] = propertyValue;
-        }
+export function updateConfig(propertyToUpdate: string, propertyValue: Value){
+    // Do nothing if propertyValue is empty or null
+    if (!propertyValue || (Array.isArray(propertyValue) && propertyValue.length === 0)) {
+        console.log(`propertyValue is empty or null, no update performed.`);
+        return;
+    }
 
-        // convert the javascript object back to yaml
-        let yamlStr: string = yaml.dump(config);
-
-        // write the updated config back to the file
-        fs.writeFileSync('./path/to/your/config.yaml', yamlStr, 'utf8');
-        
-    } catch(e) {
-        console.log(e);
+    if (propertyToUpdate in doc.suites[0].env) {
+        (doc.suites[0].env as any)[propertyToUpdate] = propertyValue;
+    } else if (propertyToUpdate === 'metadata.build') {
+        doc.sauce.metadata.build = propertyValue as string;  // We assume that build should always be a string
+    } else if (propertyToUpdate === 'metadata.tags') {
+        doc.sauce.metadata.tags = propertyValue as string[];  // We assume that tags should always be an array of strings
+    } else {
+        console.error('Invalid property to update');
     }
 }
+
+
+
+
+
+
